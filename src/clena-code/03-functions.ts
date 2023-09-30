ac511ca4b498856d92bd63ad8976cd2971278bc3
juanpa610 <jaramilloorregojuanpablo@gmail.com>
2023-09-30 16:41:54 -0500

    evitar el uso del else

--- src/clena-code/03-functions.ts
+++ src/clena-code/03-functions.ts
@@ -35,7 +35,42 @@
 
     }
 
-   
+    // Continuacion: 
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    const getPayAmountV1 = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
+        if ( isDead ) return 1500;
+        if ( isSeparated) return 2500;
+        return  isRetired  ? 3000 : 400;
+    }
+
+    const getPayAmountV2 = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
+        if ( isDead ) return 1500;
+        if ( isSeparated) return 2500;
+        if ( isRetired ) return 300;
+        return 400;
+    }
+
+    const getPayAmountV3 = ({ isDead = false, isSeparated = true, isRetired = false }):number => 
+      isDead ? 1500 : isSeparated ? 2500 : isRetired ? 3000 : 4000;
 
 })();
 
