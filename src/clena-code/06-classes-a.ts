3d7e3ad438f87397406976f0c6706827da4bfe63
juanpa610 <jaramilloorregojuanpablo@gmail.com>
2023-10-01 14:25:50 -0500

    no se aplica SRP

--- src/clena-code/06-classes-a.ts
+++ src/clena-code/06-classes-a.ts
@@ -1,5 +1,11 @@
 (() => {
 
+    // ! SRP : Single Responsibility Principle = Principio de responsabilidad unica
+    // * en ingeniería de software establece que cada módulo o clase debe tener responsabilidad 
+    // * sobre una sola parte de la funcionalidad proporcionada por el software y esta 
+    // * responsabilidad debe estar encapsulada en su totalidad por la clase. Todos sus servicios 
+    // * deben estar estrechamente alineados con esa responsabilidad.
+
     type Gender = 'F'|'M';
     class Person {
 
@@ -19,8 +25,47 @@
         // ! Forma Corta 
         constructor(public name: string, public gender: Gender, public age: number, public birtDate: Date) { }
     }
+    
+    class User extends Person {
+        private lastAccess : Date;
 
-    const newPerson = new Person('Juan Pablo Jaramilo Orrego', 'M', 19, new Date(2004, 1, 26));
-    console.log( newPerson );
+        constructor( 
+            public email : string , 
+            public role : string, 
+            name : string,
+            gender : Gender,
+            age : number,
+            birtDate : Date
+        ){
+            super(name, gender, age, birtDate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredential(){
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory : string,
+            public lastOpenFolder : string,
+            email : string,
+            role : string,
+            name : string,
+            gender : Gender,
+            age : number,
+            birtDate : Date
+        ){
+            super( email, role ,name, gender, age, birtDate);
+        }
+    }
+
+    // const newPerson = new Person('Juan Pablo Jaramilo Orrego', 'M', 19, new Date(2004, 1, 26));
+
+    const newUser = new User('juan.jaramilo@pragma.com.co', 'user', 'Juan', 'F', 12, new Date(2004, 1, 26)  )
+    
+    const newUserSettings2 = new UserSettings('/user/home', '/home', 'juan.jaramilo@pragma.com.co', 'user', 'Juan', 'F', 12, new Date(2004, 1, 26) );
+    console.log( {newUserSettings2}  );
    
 })();
